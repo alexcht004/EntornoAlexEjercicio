f3eb5637c31a5fdf68fc6b4fde33cc70290b1ce5
alexcht004 <56443132+alexcht004@users.noreply.github.com>
2020-05-10 21:21:02 +0200

    Add files via upload

---
+++ diagramaClases/src/diagramaClases/SalasDeReunion.java
@@ -0,0 +1,15 @@
+package diagramaClases;
+
+public class SalasDeReunion	extends Instalaciones{
+
+
+		String nombre;
+		int dimencion ;
+		String estado;
+	    public SalasDeReunion(){}
+	    public SalasDeReunion(String nombre, int dimencion, String estado){
+	        this.nombre = nombre;
+	        this.dimencion = dimencion;
+	  
+	    }	
+	}
